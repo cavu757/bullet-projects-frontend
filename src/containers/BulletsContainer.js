cef81152d91a1ec105e4062ffc6285dab97f093c
cavu757 <jose.pehovazdiez@gmail.com>
2020-01-05 21:19:50 -0500

    created bullets container

---
+++ src/containers/BulletsContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class BulletsContainer extends React.Component {
+
+  render(){
+    return(
+      <div>
+        BulletsContainer
+      </div>
+    )
+  }
+}
+
+export default BulletsContainer
