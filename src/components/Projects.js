12b945a4c400941c9c4a50b293d52ef945b6de38
cavu757 <jose.pehovazdiez@gmail.com>
2020-01-07 23:19:21 -0500

    updated formatting for days left

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,12 +2,12 @@ import React from 'react';
 import {Link} from 'react-router-dom'
 
 const Projects = (props) => {
-  
+
   return (
     <div>
       {props.projects.map(project =>
         <div key={project.id}>
-          <Link to={`/projects/${project.id}`}>{project.name}</Link>
+          <Link to={`/projects/${project.id}`}>{project.name}</Link> (Days Left: {project.days_left})
         </div>)}
     </div>
   )
