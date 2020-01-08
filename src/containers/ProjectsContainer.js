3799874c001bac75f1c3d59d80931c1b30080b71
cavu757 <jose.pehovazdiez@gmail.com>
2020-01-07 23:20:08 -0500

    added fetch events for dashboard

--- src/containers/ProjectsContainer.js
+++ src/containers/ProjectsContainer.js
@@ -4,12 +4,15 @@ import Projects from '../components/Projects';
 import Project from '../components/Project';
 import { connect } from 'react-redux';
 import {fetchProjects} from '../actions/fetchProjects';
+import {fetchAllEvents} from '../actions/fetchAllEvents';
 import { Route, Link } from 'react-router-dom'
+import EventsContainer from './EventsContainer'
 
 class ProjectsContainer extends React.Component {
 
   componentDidMount(){
-    this.props.fetchProjects()
+    this.props.fetchProjects();
+    this.props.fetchAllEvents()
   }
 
   render(){
@@ -17,19 +20,22 @@ class ProjectsContainer extends React.Component {
       <div>
         <Link to={'/'}>Home</Link> *** <Link to={'/projects'}>All projects</Link> *** <Link to={'/projects/new'}>New project</Link>
         <br></br>
-        <br></br>
         <Route path='/projects/new' component={ProjectInput}/>
         <Route path='/projects/:id' render={(routerProps) => <Project {...routerProps} projects={this.props.projects}/>} />
         <Route exact path='/projects' render={(routerProps) => <Projects {...routerProps} projects={this.props.projects}/>} />
+        <Route exact path='/' render={(routerProps) => <EventsContainer {...routerProps} events={this.props.events}/>} />
+
       </div>
     )
   }
 }
 
 const mapStateToProps = state => {
+
   return {
-    projects: state.projects
+    projects: state.projects,
+    events: state.events
   }
 }
 
-export default connect(mapStateToProps, {fetchProjects})(ProjectsContainer)
+export default connect(mapStateToProps, {fetchProjects, fetchAllEvents})(ProjectsContainer)
