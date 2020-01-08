400e1658aa63d4f86383016c1229f2ebfb9211ca
cavu757 <jose.pehovazdiez@gmail.com>
2020-01-07 23:19:41 -0500

    updated format

--- src/containers/BulletsContainer.js
+++ src/containers/BulletsContainer.js
@@ -7,8 +7,8 @@ class BulletsContainer extends React.Component {
   render(){
     return(
       <div>
-        <BulletInput project={this.props.project}/>
         <br></br>
+        <BulletInput project={this.props.project}/>
         <Bullets bullets={this.props.project && this.props.project.bullets}/>
       </div>
     )
