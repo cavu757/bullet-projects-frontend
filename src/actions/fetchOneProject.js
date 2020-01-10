9252181ae5a3f5248536b859c0524f20aa57a3d9
cavu757 <jose.pehovazdiez@gmail.com>
2020-01-10 16:33:40 -0500

    added fetch project dispatch action

---
+++ src/actions/fetchOneProject.js
@@ -0,0 +1,11 @@
+
+export function fetchOneProject(projectId) {
+  return (dispatch) => {
+    fetch(`http://localhost:3000/api/v1/projects/${projectId}`)
+      .then(response => response.json())
+      .then(project => dispatch({
+        type: 'FETCH_ONE_PROJECT',
+        payload: project
+      }))
+  }
+}
