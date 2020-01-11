e14aeaac4e9b1179bdbdd58234c2badbc6d1ea4a
cavu757 <jose.pehovazdiez@gmail.com>
2020-01-11 14:57:24 -0500

    two actions for fetching one bullet and project
    future functionality maybe

---
+++ src/actions/fetchOneBullet.js
@@ -0,0 +1,13 @@
+
+// Not required at this time
+
+// export function fetchOneBullet(projectId, bulletId) {
+//   return (dispatch) => {
+//     fetch(`http://localhost:3000/api/v1/projects/${projectId}/bullets/${bulletId}`)
+//       .then(response => response.json())
+//       .then(bullet => dispatch({
+//         type: 'FETCH_ONE_BULLET',
+//         payload: bullet
+//       }))
+//   }
+// }
--- src/actions/fetchOneProject.js
+++ src/actions/fetchOneProject.js
@@ -1,11 +1,13 @@
 
-export function fetchOneProject(projectId) {
-  return (dispatch) => {
-    fetch(`http://localhost:3000/api/v1/projects/${projectId}`)
-      .then(response => response.json())
-      .then(project => dispatch({
-        type: 'FETCH_ONE_PROJECT',
-        payload: project
-      }))
-  }
-}
+// Not required at this time
+
+// export function fetchOneProject(projectId) {
+//   return (dispatch) => {
+//     fetch(`http://localhost:3000/api/v1/projects/${projectId}`)
+//       .then(response => response.json())
+//       .then(project => dispatch({
+//         type: 'FETCH_ONE_PROJECT',
+//         payload: project
+//       }))
+//   }
+// }
