beb27576feb4352c87d8e5ab86adc2f1ae34b96a
cavu757 <jose.pehovazdiez@gmail.com>
2020-01-12 18:56:43 -0500

    added conditional to have text in case no events

--- src/components/Events.js
+++ src/components/Events.js
@@ -7,13 +7,22 @@ const Events = (props) => {
   let sortedEvents = events.sort(function(a,b){
   return new Date(a.date) - new Date(b.date)
   });
-
+  if (sortedEvents.length>0){
   return (
     <div>
+      Upcoming Events
+      <br></br>
       {sortedEvents && sortedEvents.map(e =>
-        <div key={e.id}> {e.date} - <Link to={`/projects/${e.project_id}`}>{e.content}</Link> ({e.project.name})</div>)}
+        <div key={e.id}> {e.date.slice(5)} / <Link to={`/projects/${e.project_id}`}>{e.content}</Link> ({e.project.name})</div>)}
     </div>
-  )
+  )}
+  else {
+    return (
+      <div>
+      Did not find any upcoming events :(
+      </div>
+    )
+  }
 }
 
 export default Events
