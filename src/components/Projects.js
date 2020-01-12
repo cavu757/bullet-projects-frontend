16408df21cd6b2757a28d94fd0c8c731fa018149
cavu757 <jose.pehovazdiez@gmail.com>
2020-01-12 18:58:51 -0500

    added styling to projects listing to ensure text if no projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,16 +4,23 @@ import {Link} from 'react-router-dom'
 
 const Projects = (props) => {
 
+  if (props.projects.length>0){
   return (
     <div>
       <br></br>
       {props.projects.map(project =>
         <div key={project.id}>
-          ({project.bullets.length}) <Link to={`/projects/${project.id}`}>{project.name}</Link> (Days Left: {project.days_left})
-
+          <Link to={`/projects/${project.id}`}><span className="blue">{project.bullets.length}</span> {project.name}</Link> (Days Left: {project.days_left})
         </div>)}
     </div>
+  )}
+  else {
+  return (
+    <div>
+    <br></br>Blank state, create something :)
+    </div>
   )
+  }
 }
 
 export default Projects;
